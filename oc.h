0084e3129a1111b38ebf76ee3ce630bb53ceb1bf
Hellwest <dushekrad@gmail.com>
2020-08-02 21:10:41 +0400

    use Mersenne Twister for seeding random

--- oc.h
+++ oc.h
@@ -2,12 +2,14 @@
 
 #include <cstdlib>
 #include <time.h>
+#include <random>
 
 namespace oc {
 	std::string random_sequence(int length) {
 		std::string result;
-
-		std::srand(time(NULL));
+		std::mt19937 gen;
+	
+		gen.seed(time(NULL));
 		for (int i = 0; i < length; ++i) {
 			int randomNumber = std::rand() % 26;
 			char letter = 'a' + randomNumber;
