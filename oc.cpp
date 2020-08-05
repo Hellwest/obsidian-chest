d0a7c3ec11501d8f40aea0a4037ba4376d09b790
Hellwest <dushekrad@gmail.com>
2020-08-05 23:45:43 +0400

    add list of possible characters

--- oc.cpp
+++ oc.cpp
@@ -1,20 +1,16 @@
 #include "oc.h"
 
+constexpr char OC::possible_chars[];
+
 std::string OC::random_sequence(int length) {
 		unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 		std::mt19937 generator(seed);
 
 		std::string result;
 		for (int i = 0; i < length; ++i) {
-				char x = generator() % 62;
-				if(x < 10) {
-						x+= '0';
-				} else if(x < 36) {
-						x += 'A' - 10;
-				} else {
-						x += 'a' - 36;
-				}
-				result += x;
+				int index = generator() % 62;
+				char character = OC::possible_chars[index];
+				result += character;
 		}
 
 		return result;
