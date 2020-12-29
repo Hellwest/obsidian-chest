255016c47b11b367506caf5e14fbcfe6dc5a32e0
Vladislav Nikitin <dushekrad@gmail.com>
2020-12-29 15:57:42 +0400

    feat: add read_entry method

--- source/manager.cpp
+++ source/manager.cpp
@@ -3,18 +3,56 @@
 
 #include <manager.h>
 
+char delimiter(',');
+std::string filename("./oc");
+
 void Manager::add_entry(std::string service_name, std::string user_name, std::string password) {
 	std::ofstream f;
-	f.open("./oc", std::ifstream::app);
+	f.open(filename, std::ofstream::app);
 	if (!f.is_open()) {
 		std::cout << "Error: Failed to open a file\n";
 		exit(1);
 	}
 
-	f << "Service: " << service_name << " | ";
-	f << "Username: " << user_name << " | ";
-	f << "Password: " << password << "\n";
+	f << service_name << delimiter;
+	f << user_name << delimiter;
+	f << password << std::endl;
 
 	std::cout << "Written successfully\n";
 	f.close();
+}
+
+std::string Manager::read_entry(std::string service_name) {
+	std::ifstream f;
+	f.open(filename, std::ifstream::in);
+	if (!f.is_open()) {
+		std::cout << "Error: Failed to open a file\n";
+		exit(1);
+	}
+
+	std::string line;
+	for (; std::getline(f, line);) {
+		std::size_t pos = line.find(delimiter);
+		std::string service = line.substr(0, pos);
+		if (service == service_name) {
+			break;
+		}
+	}
+
+	std::size_t service_pos = line.find(delimiter);
+	std::string service = line.substr(0, service_pos);
+
+	++service_pos; // avoid comma
+
+	std::size_t username_pos = line.find(delimiter, service_pos);
+	std::string username = line.substr(service_pos, username_pos - service_pos);
+
+	++username_pos;
+
+	std::size_t endl_pos = line.length();
+	std::string password = line.substr(username_pos, endl_pos);
+
+	f.close();
+
+	return service;
 }
