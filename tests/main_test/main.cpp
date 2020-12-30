e16f8552c995d54c0654d681f2d6abd52f669356
Hellwest <dushekrad@gmail.com>
2020-12-30 13:25:43 +0400

    add command line parameters

--- tests/main_test/main.cpp
+++ tests/main_test/main.cpp
@@ -10,16 +10,46 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	int length = std::stoi(argv[1]);
-	std::string result = OC::random_sequence(length);
-
-	std::cout << result << "\n";
-
 	Manager manager;
 
-	manager.add_entry("test_service", "test_user", result);
-	// manager.read_entry("test_service");
-	manager.remove_entry("test_service");
+	std::string command = std::string(argv[1]);
+
+	if (command == "generate") {
+		if (argc < 3) {
+			std::cout << "Provide a password length\n";
+			return -1;
+		}
+
+		int password_length = std::stoi(argv[2]);
+
+		std::string result = OC::random_sequence(password_length);
+		std::cout << result << "\n";
+
+		manager.add_entry("test_service", "test_user", result);
+	}
+
+	if (command == "read") {
+		if (argc < 3) {
+			std::cout << "Provide a service name to read\n";
+			return -1;
+		}
+
+		std::string service_name = std::string(argv[2]);
+
+		manager.read_entry(service_name);
+	}
+
+	if (command == "remove") {
+		if (argc < 3) {
+			std::cout << "Provide a service name to remove\n";
+			return -1;
+		}
+
+		std::string service_name = std::string(argv[2]);
+
+		manager.remove_entry(service_name);
+	}
 
 	return 0;
 }
+
