cdeb18a561b90c428ea249b64d3df2d5d326b939
Hellwest <dushekrad@gmail.com>
2020-12-30 13:24:48 +0400

    add additional checks

--- source/manager.cpp
+++ source/manager.cpp
@@ -8,6 +8,20 @@ char delimiter(',');
 std::string filename("./oc");
 
 void Manager::add_entry(std::string service_name, std::string user_name, std::string password) {
+	std::ifstream file_reading_stream;
+	file_reading_stream.open(filename, std::ifstream::in);
+	if (!file_reading_stream.is_open()) {
+		std::cout << "Error: Failed to open a file\n";
+		exit(1);
+	}
+	std::string line = this->find_line(file_reading_stream, service_name);
+	file_reading_stream.close();
+
+	if (line.length() > 0) {
+		std::cout << "Record exists\n";
+		exit(1);
+	}
+
 	std::ofstream f;
 	f.open(filename, std::ofstream::app);
 	if (!f.is_open()) {
@@ -33,6 +47,11 @@ std::string Manager::read_entry(std::string service_name) {
 
 	std::string line = this->find_line(f, service_name);
 
+	if (line.length() == 0) {
+		std::cout << "Record was not found\n";
+		exit(1);
+	}
+
 	std::size_t service_pos = line.find(delimiter);
 	std::string service = line.substr(0, service_pos);
 
@@ -48,6 +67,7 @@ std::string Manager::read_entry(std::string service_name) {
 
 	f.close();
 
+	std::cout << line << "\n";
 	return service;
 }
 
@@ -55,16 +75,24 @@ void Manager::remove_entry(std::string service_name) {
 	std::ifstream line_finder_stream;
 	line_finder_stream.open(filename, std::ifstream::in);
 	if (!line_finder_stream.is_open()) {
-		std::cout << "Error opening the file\n";
+		std::cout << "Error: Failed to open a file\n";
 		exit(1);
 	}
 
 	std::string line = this->find_line(line_finder_stream, service_name);
 
+	if (line.length() == 0) {
+		std::cout << "Record was not found\n";
+		exit(1);
+	}
+
 	line_finder_stream.close();
 
 	std::ifstream file_reader_stream;
 	file_reader_stream.open(filename, std::ifstream::in);
+	if (!file_reader_stream.is_open()) {
+		std::cout << "Error: Failed to open a file\n";
+	}
 	std::ofstream temp;
 	temp.open("./temp", std::ofstream::app);
 
@@ -79,15 +107,16 @@ void Manager::remove_entry(std::string service_name) {
 	file_reader_stream.close();
 	std::remove(filename.c_str());
 	std::rename("./temp", filename.c_str());
+	std::cout << "Removed successfully\n";
 }
 
 std::string Manager::find_line(std::ifstream& f, std::string service_name) {
 	for (std::string line; std::getline(f, line);) {
-		std::cout << "Line: " << line << "\n";
 		std::size_t pos = line.find(delimiter);
 		std::string service = line.substr(0, pos);
 		if (service == service_name) {
 			return line;
 		}
 	}
+	return "";
 }
